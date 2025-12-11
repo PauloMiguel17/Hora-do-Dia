c7aacc580af37d776a8ee590a767d9aa780ab17e
PauloMiguel17 <paulomiguelpaes2@gmail.com>
2025-12-11 15:47:15 -0300

    Adição da funcionalidade de carregamento dinâmico da hora e atualização da mensagem e imagem

---
+++ script/script.js
@@ -0,0 +1,19 @@
+function carregar(){
+    var msg = document.getElementById('msg');
+    var imagem = document.getElementById('foto');
+    var data = new Date();
+    var hora = data.getHours();
+    msg.innerHTML = `Agora sao ${hora} horas`
+    if(hora >= 0 && hora < 12){
+        imagem.src = 'imagens/imagem_manha-ofc.png'
+        document.body.style.background = '#FFD470'
+    }
+    if(hora >= 12 && hora < 18){
+        imagem.src = 'imagens/imagem_tarde-ofc.png'
+        document.body.style.background = '#70D3FF'
+    }
+    else{
+        imagem.src = 'imagens/imagem_noite_ofc.png'
+        document.body.style.background = '#264352'
+    }
+}
